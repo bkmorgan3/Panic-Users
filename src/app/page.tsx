80486932d7db1abe615f13388f3819e083c9fcf0
Brad Morgan <bkmorgan3@gmail.com>
2024-06-17 16:44:37 -0700

    page and style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,11 @@
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Heloo
+    <main className="w-screen h-screen bg-black flex justify-center items-center text-white">
+      <div className="w-full max-w-[600px] mx-auto">
+        <h1 className="text-6xl mb-4">Users App</h1>
+        <p className="text-2xl text-white/60 mb-4">Here we go</p>
+      </div>
     </main>
   );
 }
