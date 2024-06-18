5b391401ba596278fe4b9518f34e1f1e4052ff50
Brad Morgan <bkmorgan3@gmail.com>
2024-06-17 21:32:28 -0700

    add google site verification in tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta name="google-site-verification" content="google4a2b699c508ec1af.html" />
+      </head>
       <body className={inter.className}>
         <header>
           <nav>
