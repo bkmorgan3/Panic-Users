d52e6322e20c5ecbfe754e98c533bb0d4a521b56
Brad Morgan <bkmorgan3@gmail.com>
2024-07-25 16:26:12 -0700

    move keys

--- lib/sheets.ts
+++ lib/sheets.ts
@@ -4,7 +4,9 @@ import { prisma } from "./prisma"
 
 export async function getSheetsData() {
     const jwt = new google.auth.JWT(
-        keys.client_email, '', keys.private_key.replace(/\\n/g, 'n'), ['https://www.googleapis.com/auth/spreadsheets.readonly']
+        process.env.client_email, 
+        '',
+         process.env.private_key.replace(/\\n/g, 'n'), ['https://www.googleapis.com/auth/spreadsheets.readonly']
     )
     const sheets = google.sheets({version: "v4", auth: jwt})
     const range = "Form Responses 1!A2:J152"
