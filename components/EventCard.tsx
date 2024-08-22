d76f634cd151c79a27c32499218315e0983ccf0e
Brad Morgan <bkmorgan3@gmail.com>
2024-08-22 14:48:44 -0700

    link to eventpage

--- components/EventCard.tsx
+++ components/EventCard.tsx
@@ -1,9 +1,15 @@
-
+import Link from "next/link"
 
 export const EventCard = ({event}) => {
+    
     return (
         <div className="w-[250px] h-[250px] border-2 border-sky-500 mx-3.5 mt-2 p-2">
-            <span>{event.name}</span>
+            <Link href={`/events/${event.id}`}><span>{event.name}</span></Link>
+            <span>{event.address}</span>
+            <span>{event.address2}</span>
+            <span>{event.city}</span>
+            <span>{event.state}</span>
+            {/* <span>{event.date}</span> */}
         </div>
     )
 }
--- src/app/(dashboard)/events/[id]/page.tsx
+++ src/app/(dashboard)/events/[id]/page.tsx
@@ -1,9 +1,32 @@
+import { prisma } from "../../../../../lib/prisma"
 
+const getUsersFromEvent = async (id: string) => {
+    console.log("fetch", id)
+    const event = await prisma.user.findMany({
+        where: {
+            eventId: id
+        }
+    })
+    return event
+}
 
-export default async function EventPage() {
+const getEventData = async (id: string) => {
+    const event = await prisma.event.findUnique({
+        where: {
+            id
+        }
+    })
+    return event
+}
+
+export default async function EventPage({params}) {
+   const {id} = params
+    const users = await getUsersFromEvent(id)
+    const eventData = await getEventData(id)
+    console.log(eventData)
     return (
         <div>
-            Event page
+            <h2>{eventData.name}</h2>
         </div>
     )
 }
